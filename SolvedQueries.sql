5197d03a915c09fcdda76bfdc5ea259a05a98436
ManivasaganJegadesh <manivasaganjegadesh2604@gmail.com>
2025-01-17 13:45:48 +0530

    SolvedQueries.sql

--- SolvedQueries.sql
+++ SolvedQueries.sql
@@ -1,98 +1,111 @@
 SELECT * FROM mortgage_loans;
 
+## Easy Queries
 -- 1. Total Number of Loans in the Database
-Select count(*) as Total_Loans
-from mortgage_loans;
+SELECT COUNT(*) AS Total_Loans FROM mortgage_loans;
+
 
 -- 2. Loan Count by State
-Select state, count(*) as total_loan_count
-from mortgage_loans
-group by state
-order by total_loan_count desc;
+SELECT state, COUNT(*) AS total_loan_count
+FROM mortgage_loans
+GROUP BY state
+ORDER BY total_loan_count DESC;
+
 
 -- 3. Loan Approval Rate
-select 
-sum(case when loan_status = 'Approved' then 1 else 0 end)*100/count(*) as approval_rate
-from mortgage_loans;
+SELECT 
+    SUM(CASE WHEN loan_status = 'Approved' THEN 1 ELSE 0 END) * 100 / COUNT(*) AS approval_rate
+FROM mortgage_loans;
+
 
 -- 4. Total Loan Amount by Loan Status (Approved vs. Rejected)
-select loan_status, format(sum(loan_amount),0) as total_loan_Amount
-from mortgage_loans
-group by loan_status;
+SELECT loan_status, FORMAT(SUM(loan_amount), 0) AS total_loan_Amount
+FROM mortgage_loans
+GROUP BY loan_status;
 
--- 5.Average Interest Rate Based on Credit Score Ranges
-select 
-case 	
-	when credit_score >= 750 then 'Excellent (>=750)'
-    when credit_score between 700 and 749 then 'Good (700-749)'
-    when credit_score between 650 and 699 then 'Fair (650-699)'
-    else 'Poor (<650)'
-    end as credit_category,
-round(avg(interest_rate),2) as avg_rate,
-count(*) as num_loans
-from mortgage_loans
-group by credit_category
-order by avg_rate desc;
 
--- 6. What is the approval rate of loans by state?
-select state, count(*) as Approved_loan,
-round(count(*)*100/(select count(*) from mortgage_loans),2) as Approval_Rate
-from mortgage_loans
-where loan_status = 'Approved'
-group by state;
+-- 5.Loan Approval Rate by State
+SELECT state, COUNT(*) AS Approved_loan,
+ROUND(COUNT(*) * 100 / (SELECT COUNT(*) FROM mortgage_loans), 2) AS Approval_Rate
+FROM mortgage_loans
+WHERE loan_status = 'Approved'
+GROUP BY state;
 
--- 7.  Which states have the highest loan approval rates?
-select state,count(*) as total_loans,
-sum(case when loan_status = 'Approved' then 1 else 0 end) as approved_loans,
-round(sum(case when loan_status = 'Approved' then 1 else 0 end)*100/count(*),2) as approval_rate
-from mortgage_loans
-group by state
-order by approval_rate desc;
 
--- 8. Monthly Loan Applications Trend
-select date_format(application_date, '%y-%m') as month_year,
-count(*) as total_loans
-from mortgage_loans
-group by month_year
-order by month_year;
+-- 6. Loans Approved per Month
+SELECT DATE_FORMAT(application_date, '%y-%m') AS month_year, COUNT(*) AS total_count
+FROM mortgage_loans
+WHERE loan_status = 'Approved'
+GROUP BY month_year
+ORDER BY month_year;
 
--- 9. Maximum Loan Amount by Credit Score Range
-select 
-case
-	when credit_score >= 750 then 'Excellent (>=750)'
-    when credit_score between 700 and 749 then 'Good (700-749)'
-    when credit_score between 650 and 699 then 'Fair (650-699)'
-    else 'Poor (<650)'
-    end as credit_category, 
-max(loan_amount) as max_loan_amount
-from mortgage_loans
-group by credit_category;
 
--- 10. Loans Approved per Month
-select date_format(application_date,'%y-%m') as month_year, count(*) as total_count
-from mortgage_loans
-where loan_status = 'Approved'
-group by month_year
-order by month_year;
+-- 7.  Loans by Purpose (Home Purchase, Refinance, etc.)
+SELECT loan_purpose, COUNT(*) AS loan_count
+FROM mortgage_loans
+GROUP BY loan_purpose
+ORDER BY loan_count DESC;
 
--- 11. Loans by Purpose (Home Purchase, Refinance, etc.)
-select loan_purpose, count(*) as loan_count
-from mortgage_loans
-group by loan_purpose
-order by loan_count desc;
 
--- 12.Average Interest Rate by Loan Purpose
-select loan_purpose, round(avg(interest_rate),2) as avg_interest_rate
-from mortgage_loans
-group by loan_purpose;
+-- 8. Average Interest Rate by Loan Purpose
+SELECT loan_purpose, ROUND(AVG(interest_rate), 2) AS avg_interest_rate
+FROM mortgage_loans
+GROUP BY loan_purpose;
 
--- 13.Loans with Credit Score Above 700 and Approved Status
-select borrower_name, loan_amount, credit_score, interest_rate, application_date
-from mortgage_loans
-where credit_score > 700 and loan_status = 'Approved';
 
--- 14. Loans in the Last Year
-select *
-from mortgage_loans
-where application_date > current_date() - interval 1 year
+-- 9.  Loans with Credit Score Above 700 and Approved Status
+SELECT borrower_name, loan_amount, credit_score, interest_rate, application_date
+FROM mortgage_loans
+WHERE credit_score > 700 AND loan_status = 'Approved';
+
+
+-- 10. Loans in the Last Year
+SELECT * FROM mortgage_loans
+WHERE application_date > CURRENT_DATE() - INTERVAL 1 YEAR;
+
+## Medium Queries
+
+-- 11. Average Interest Rate Based on Credit Score Ranges
+SELECT 
+    CASE 	
+        WHEN credit_score >= 750 THEN 'Excellent (>=750)'
+        WHEN credit_score BETWEEN 700 AND 749 THEN 'Good (700-749)'
+        WHEN credit_score BETWEEN 650 AND 699 THEN 'Fair (650-699)'
+        ELSE 'Poor (<650)'
+    END AS credit_category,
+    ROUND(AVG(interest_rate), 2) AS avg_rate,
+    COUNT(*) AS num_loans
+FROM mortgage_loans
+GROUP BY credit_category
+ORDER BY avg_rate DESC;
+
+
+-- 12.Which States Have the Highest Loan Approval Rates?
+SELECT state, COUNT(*) AS total_loans,
+SUM(CASE WHEN loan_status = 'Approved' THEN 1 ELSE 0 END) AS approved_loans,
+ROUND(SUM(CASE WHEN loan_status = 'Approved' THEN 1 ELSE 0 END) * 100 / COUNT(*), 2) AS approval_rate
+FROM mortgage_loans
+GROUP BY state
+ORDER BY approval_rate DESC;
+
+
+-- 13.Monthly Loan Applications Trend
+SELECT DATE_FORMAT(application_date, '%y-%m') AS month_year,
+COUNT(*) AS total_loans
+FROM mortgage_loans
+GROUP BY month_year
+ORDER BY month_year;
+
+
+-- 14. Maximum Loan Amount by Credit Score Range
+SELECT 
+    CASE
+        WHEN credit_score >= 750 THEN 'Excellent (>=750)'
+        WHEN credit_score BETWEEN 700 AND 749 THEN 'Good (700-749)'
+        WHEN credit_score BETWEEN 650 AND 699 THEN 'Fair (650-699)'
+        ELSE 'Poor (<650)'
+    END AS credit_category, 
+    MAX(loan_amount) AS max_loan_amount
+FROM mortgage_loans
+GROUP BY credit_category;
 
